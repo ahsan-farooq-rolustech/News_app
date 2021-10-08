54e3347e6c0d2743e9483375dac87d27d9888d92
Ahsan Farooq <ahsanfarooq210@gmail.com>
2021-10-08 00:41:16 -0700

    headlines front end

--- app/src/main/java/com/example/seriousnewsapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/seriousnewsapp/activity/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.seriousnewsapp
+package com.example.seriousnewsapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import com.example.seriousnewsapp.R
 import com.example.seriousnewsapp.application.NewsApplication
 import com.example.seriousnewsapp.viewModel.MainViewModel
 import com.example.seriousnewsapp.viewModel.MainViewModelFactory
